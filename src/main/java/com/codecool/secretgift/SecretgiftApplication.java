86819ce4ddb2f68f218f71988057c5a0324dc9df
iulian-coder <iulian.titei@gmail.com>
2020-12-14 11:31:13 +0200

    first commit

---
+++ src/main/java/com/codecool/secretgift/SecretgiftApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.secretgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretgiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretgiftApplication.class, args);
+    }
+
+}
